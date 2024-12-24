f2519c437aeaed3284cda954b74aaaef966e0285
OriginalKimChi <pjhyl1127@gmail.com>
2024-12-24 19:07:06 +0000

    refactor layout and improve sustainability quiz feedback

--- end.js
+++ end.js
@@ -6,7 +6,7 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const MAX_HIGH_SCORES = 80;
+const MAX_HIGH_SCORES = 8;
 
 getScore();
 
@@ -16,16 +16,21 @@ username.addEventListener('keyup', () => {
 
 function getScore() {
     if (mostRecentScore >= MAX_HIGH_SCORES) {
-        scoreTextHigh.innerText = "Congratuation!\nYou are familiar with Nottingham Forest FC's sustainability policy.";
-        scoreTextLow.style = "display : none";
+        scoreTextHigh.innerHTML = "You Did It! A True Forest Fan. With a fantastic score of<br>" + 
+            "<strong style='color: red;'> " + mostRecentScore + "/10 " + "</strong><br>" +
+            "you’ve shown you truly know what Nottingham Forest FC stands for—on and off the pitch!";
+        guide1.innerText = "Your passion for the Reds and sustainability makes a real difference. Let’s keep building a greener future together."
+        guide2.innerText = "Want to stay ahead of the game? Learn more about our sustainability goals by visiting our official page."
         if (firstScore >= MAX_HIGH_SCORES) {
             couponGuide.style = "display : inline; color: black; text-align: center; margin-bottom: 30px;";
             generateBarcode();
         }
     } else {
-        scoreTextHigh.style = "display : none";
+        scoreTextHigh.innerHTML = "Thank You for Taking Part! You scored<br>" + 
+            "<strong style='color: red;'> " + mostRecentScore + "/10 " + "</strong><br>" +
+            "— just a little shy of earning a free drink this time.";
     }
-    finalScore.innerText = mostRecentScore + "/100";
+    finalScore.innerText = mostRecentScore + "/10";
 }
 
 function generateBarcode() {
