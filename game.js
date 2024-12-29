84fbaaab878b9a8ee9fc2edf7d9526dd28a7f48a
OriginalKimChi <pjhyl1127@gmail.com>
2024-12-29 19:08:15 +0000

    improve answer choice validation for quiz questions

--- game.js
+++ game.js
@@ -203,7 +203,7 @@ try {
 
         const answerChoices = [...loadedQuestion.incorrect_answers];
 
-        if (loadedQuestion.correct_answer === "All of the above" || loadedQuestion.correct_answer === "None of the above") {
+        if (loadedQuestion.correct_answer === "All of the above" || answerChoices.find((choice) => choice === "None of the above")) {
             formattedQuestion.answer = answerChoices.length + 1;
             answerChoices.push(loadedQuestion.correct_answer);
         } else {
